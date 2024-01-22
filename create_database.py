01fb0c42e35ddf5ccc7e9ab4a3f0fdad725514b2
RajK-NeetiAI <raj@neeti.ai>
2024-01-22 12:15:10 +0530

    added functionality to handle date

--- create_database.py
+++ create_database.py
@@ -1,6 +1,6 @@
 import re
-import json
 from typing import Any
+import json
 
 import pandas as pd
 from sqlalchemy import create_engine
@@ -19,9 +19,38 @@ def clean_column_names(column_name: str) -> str:
     return cleaned_name
 
 
+def is_valid_date(col: list) -> bool:
+    date_string = str(col[0])
+    patterns = [
+        r'^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$',  # YYYY-MM-DD
+        # DD-MMM-YYYY
+        r'^(0[1-9]|[12][0-9]|3[01])-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-\d{4}$',
+        # MMM DD YYYY
+        r'^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (0[1-9]|[12][0-9]|3[01]) \d{4}$',
+        # YYYY-MMM-DD
+        r'^\d{4}-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-(0[1-9]|[12][0-9]|3[01])$',
+        # YYYY-MM-DD HH:MM:SS
+        r'^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]) (\d{2}):(\d{2}):(\d{2})$',
+        # DD-MMM-YYYY HH:MM:SS
+        r'^(0[1-9]|[12][0-9]|3[01])-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-\d{4} (\d{2}):(\d{2}):(\d{2})$',
+        # MMM DD YYYY HH:MM:SS
+        r'^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (0[1-9]|[12][0-9]|3[01]) \d{4} (\d{2}):(\d{2}):(\d{2})$',
+        # YYYY-MMM-DD HH:MM:SS
+        r'^\d{4}-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-(0[1-9]|[12][0-9]|3[01]) (\d{2}):(\d{2}):(\d{2})$',
+        # DD-MM-YYYY HH:MM:SS
+        r'^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-\d{4} (\d{2}):(\d{2}):(\d{2})$',
+    ]
+    for pattern in patterns:
+        if re.match(pattern, date_string):
+            return True
+    return False
+
+
 def convert_to_datetime(col: str) -> Any:
     try:
-        return pd.to_datetime(col)
+        if is_valid_date(col):
+            return pd.to_datetime(col)
+        return col
     except ValueError:
         return col
 
