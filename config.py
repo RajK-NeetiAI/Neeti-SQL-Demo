0c3922364ffa6e1a61c49eb028cdb8b859a9993f
RajK-NeetiAI <raj@neeti.ai>
2024-02-07 17:02:29 +0530

    added configuration to get table names

--- config.py
+++ config.py
@@ -10,5 +10,11 @@ MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 MYSQL_HOST = os.getenv('MYSQL_HOST')
 MYSQL_PORT = os.getenv('MYSQL_PORT')
 MYSQL_DB_NAME = os.getenv('MYSQL_DB_NAME')
-MYSQL_TABLES = os.getenv('MYSQL_TABLES').split(',')
+MYSQL_TABLES = []
 GPT_MODEL = os.getenv('GPT_MODEL')
+
+DEFINITION_DIR = 'data_definition'
+
+for definition in os.listdir(DEFINITION_DIR):
+    table_name = definition.rsplit('.', 1)[0]
+    MYSQL_TABLES.append(table_name)
