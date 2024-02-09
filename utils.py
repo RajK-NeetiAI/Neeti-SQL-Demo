7465b5fcabc34f201023e15106652e5880987e05
RajK-NeetiAI <raj@neeti.ai>
2024-02-09 12:35:39 +0530

    fixed mysql connection issue with cnx close

--- utils.py
+++ utils.py
@@ -3,13 +3,13 @@ import json
 import datetime
 import os
 
-from mysql.connector.pooling import MySQLConnectionPool
+from mysql.connector.pooling import MySQLConnectionPool, PooledMySQLConnection
 from mysql.connector import Error
 
 import config
 
 
-def connect_to_mysql_pool():
+def connect_to_mysql_pool() -> MySQLConnectionPool | None:
     try:
         connection_pool = MySQLConnectionPool(
             pool_name="neeti-sql",
@@ -20,26 +20,29 @@ def connect_to_mysql_pool():
             user=config.MYSQL_USER,
             password=config.MYSQL_PASSWORD
         )
-
         return connection_pool
     except Error as e:
         print(f"Error creating MySQL connection pool: {e}")
-
         return None
 
 
-def close_mysql_pool(pool: MySQLConnectionPool):
-    if pool:
-        pool.close()
-        print("MySQL connection pool closed")
+def get_mysql_pooled_cnx() -> PooledMySQLConnection | None:
+    pool = connect_to_mysql_pool()
+    if not pool:
+        return None
+    cnx = pool.get_connection()
+    return cnx
+
+
+def close_mysql_pooled_cnx(cnx: PooledMySQLConnection) -> None:
+    if cnx:
+        cnx.close()
+        print("MySQL pooled connection closed.")
 
 
 def get_table_names() -> list[str]:
     """Return a list of table names."""
-    pool = connect_to_mysql_pool()
-    if not pool:
-        return []
-    cnx = pool.get_connection()
+    cnx = get_mysql_pooled_cnx()
     cursor = cnx.cursor()
     table_names = []
     cursor.execute(
@@ -48,16 +51,13 @@ def get_table_names() -> list[str]:
         if table[0] in config.MYSQL_TABLES:
             table_names.append(table[0])
     cursor.close()
-
+    close_mysql_pooled_cnx(cnx)
     return table_names
 
 
 def get_column_names(table_name: str) -> list[str]:
     """Return a list of column names."""
-    pool = connect_to_mysql_pool()
-    if not pool:
-        return []
-    cnx = pool.get_connection()
+    cnx = get_mysql_pooled_cnx()
     cursor = cnx.cursor()
     column_names = []
     cursor.execute(
@@ -65,16 +65,13 @@ def get_column_names(table_name: str) -> list[str]:
     for col in cursor:
         column_names.append(col[0])
     cursor.close()
-
+    close_mysql_pooled_cnx(cnx)
     return column_names
 
 
 def get_database_info() -> dict:
     """Return a list of dicts containing the table name and columns for each table in the database."""
-    pool = connect_to_mysql_pool()
-    if not pool:
-        return {}
-    cnx = pool.get_connection()
+    cnx = get_mysql_pooled_cnx()
     cursor = cnx.cursor()
     table_dicts = []
     for table_name in get_table_names():
@@ -82,15 +79,11 @@ def get_database_info() -> dict:
         table_dicts.append(
             {"table_name": table_name, "column_names": columns_names})
     cursor.close()
-
+    close_mysql_pooled_cnx(cnx)
     return table_dicts
 
 
-def get_database_schema_string():
-    pool = connect_to_mysql_pool()
-    if not pool:
-        return ''
-    cnx = pool.get_connection()
+def get_database_schema_string() -> str:
     database_schema_dict = get_database_info()
     database_schema_string = "\n".join(
         [
@@ -98,26 +91,20 @@ def get_database_schema_string():
             for table in database_schema_dict
         ]
     )
-
     return database_schema_string
 
 
-def get_database_definitions():
+def get_database_definitions() -> dict:
     database_definitions = {
         "tables": []
     }
-    pool = connect_to_mysql_pool()
-    if not pool:
-        return database_definitions
-    cnx = pool.get_connection()
     for definition in os.listdir(config.DEFINITION_DIR):
         with open(os.path.join(config.DEFINITION_DIR, definition), 'r') as file:
             database_definitions['tables'].append(json.loads(file.read()))
-
     return database_definitions
 
 
-def format_number(amount):
+def format_number(amount) -> str:
     def truncate_float(number, places):
         return int(number * (10 ** places)) / 10 ** places
     if amount < 1e3:
@@ -138,12 +125,9 @@ def serialize_data(obj) -> str:
     raise TypeError(f"Object type not serializable - {type(obj)}")
 
 
-def ask_database(query: str):
+def ask_database(query: str) -> str:
     """Function to query SQLite database with a provided SQL query."""
-    pool = connect_to_mysql_pool()
-    if not pool:
-        return ''
-    cnx = pool.get_connection()
+    cnx = get_mysql_pooled_cnx()
     try:
         cursor = cnx.cursor(dictionary=True)
         cursor.execute(query)
@@ -153,14 +137,13 @@ def ask_database(query: str):
             results += json.dumps(data, default=serialize_data)
     except Exception as e:
         print(e.args[0])
-        result = ''
-        return result
-
+        results = ''
+    finally:
+        close_mysql_pooled_cnx(cnx)
     return results
 
 
 def execute_function_call(query: str) -> str:
     results = ask_database(query)
     print(results)
-
     return results
