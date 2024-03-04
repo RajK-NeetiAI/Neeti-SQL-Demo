1d0ea37f5b701dda1474face7fa565c395abe8f9
RajK-NeetiAI <raj@neeti.ai>
2024-03-04 12:05:15 +0530

    updated prompts

--- prompts.py
+++ prompts.py
@@ -1,4 +1,4 @@
-def get_sql_tool(database_schema_string: str, database_definitions: str) -> list[dict]:
+def get_sql_tool(database_schema_string: str, database_definitions: str) -> list[dict[str, str]]:
     sql_tool = [
         {
             "type": "function",
@@ -24,32 +24,29 @@ following data definitions instead: \
             }
         }
     ]
-
     return sql_tool
 
 
-def get_chat_completion_prompt(query: str, formated_chat_history: list[dict]) -> str:
-    chat_completion_prompt = f'''Consider yourselk as a helpful data analyst. A user has asked a \
+def get_chat_completion_prompt(query: str, formated_chat_history: list[dict[str, str]]) -> str:
+    chat_completion_prompt = f'''Consider yourself as a helpful data analyst of Neeti.ai. A user has asked a \
 question: {query}, in the context of the following chat history: {formated_chat_history}, politely reply \
 that you don't have the answer for the question.'''
-
     return chat_completion_prompt
 
 
-def get_format_sql_response_messages(sql_response: str) -> list[dict]:
+def get_format_sql_response_messages(sql_response: str) -> list[dict[str, str]]:
     formatted_sql_response_messages = [
-        {"role": "system", "content": "Consider yourself as a helpful data analyst. \
+        {"role": "system", "content": "Consider yourself as a helpful data analyst of Neeti.ai. \
+You can get analysis of BRS production, electrical, mechanical, and other utility maintenace report. \
 You help user get information about the data and answer their question."},
         {"role": "user", "content": f"""Convert the following MySQL data into natural language conversation, \
 keep the response short and concise and never mention id of the MySQL data. \
 SQL data: {sql_response}"""}
     ]
-
     return formatted_sql_response_messages
 
 
-def get_chat_completion_request_system_message() -> dict:
-    system_message = {"role": "system", "content": "You are a data analyst. You help user get information \
-about the database."}
-
+def get_chat_completion_request_system_message() -> dict[str, str]:
+    system_message = {"role": "system", "content": "You are a helpful data analyst of Neeti.ai. \
+You help user get information about the database."}
     return system_message
